ac02a065c2089e13bb05dd172f894e4e35af09ba
waseem saleem <mw060330@gmail.com>
2026-08-10 06:51:05 +0000

    JS Assigment

---
+++ app.js
@@ -0,0 +1,22 @@
+for (let i = 0; i <= 10; i++) {
+    console.log(i)
+}
+
+for (let i = 2; i <= 20; i += 2) {
+    console.log(i);
+}
+
+
+let array = ['apple', 'banana', 'mango', 'pineapple'];
+
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i])
+}
+
+let numbers = [40, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 40) {
+        console.log(numbers[i])
+    }
+}
