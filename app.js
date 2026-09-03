7b9e27c36312146a8c0486228346fc76ff2bfc83
waseem saleem <mw060330@gmail.com>
2026-09-03 17:28:46 +0000

    Function assigment

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
-for (let i = 0; i <= 10; i++) {
-    console.log(i)
+function myFunction() {
+    console.log("Hello world!");
 }
 
-for (let i = 2; i <= 20; i += 2) {
-    console.log(i);
+myFunction();
+
+
+function concatTwoWords(name, fatherName) {
+    console.log(name + " " + fatherName);
 }
 
+concatTwoWords("waseem", "saleem");
 
-let array = ['apple', 'banana', 'mango', 'pineapple'];
+let fruits = ["Apple", "Banana", "Mango", "Orange"]; 
 
-for (let i = 0; i < array.length; i++) {
-    console.log(array[i])
-}
-
-let numbers = [40, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-
-for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] > 40) {
-        console.log(numbers[i])
+function printFruits(fruits) {
+    for (let i = 0; i < fruits.length; i++) {
+        console.log(fruits[i]);
     }
 }
+
+printFruits(fruits);
+
+
+
+let numbers = [10, 15, 22, 31, 40];
+
+function checkNumbers(numbers) {
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 === 0) {
+            console.log(numbers[i] + " is Even");
+        } else {
+            console.log(numbers[i] + " is Odd");
+        }
+    }
+}
+
+checkNumbers(numbers);
