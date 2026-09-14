9cc760921e77a715d6503a55923fadf21c059096
waseem saleem <mw060330@gmail.com>
2026-09-14 13:03:31 +0500

    save dowhile assigment
    Comment out all function definitions and calls in app.js.

--- app.js
+++ app.js
@@ -1,26 +1,59 @@
-function userName(name) {
-    console.log("Hello " + name);
+// function assigment 
+
+// function userName(name) {
+//     console.log("Hello " + name);
     
-}
+// }
 
-userName("Waseem")
+// userName("Waseem")
 
-function adWord(word1, word2){
-    console.log(word1+ " " + word2);
-}
+// function adWord(word1, word2){
+//     console.log(word1+ " " + word2);
+// }
 
-adWord("Waseem", "Saleem")
+// adWord("Waseem", "Saleem")
 
-function multiply(num1, num2){
-    return num1 * num2
-}
+// function multiply(num1, num2){
+//     return num1 * num2
+// }
 
-let res = multiply(5, 9);
-console.log(res);
+// let res = multiply(5, 9);
+// console.log(res);
 
-function calculateSquare(number) {
-    return number * number
-}
+// function calculateSquare(number) {
+//     return number * number
+// }
 
-let square = calculateSquare(9);
-console.log(square);
+// let square = calculateSquare(9);
+// console.log(square);
+
+
+// Do while assigment 
+
+// function doWhile(number){
+//     let i = 10;
+//     do {
+//         console.log(i);
+//         i++
+//     } while (i <= number)
+// }
+
+// doWhile(15)
+
+
+// let i = 1;
+
+// do {
+//     if (i % 2 !== 0){
+//         console.log(i);
+//     }
+//     i++
+// } while(i <= 20);
+
+// let studentsName = ["hassan", "sumair", "waseem", "haseeb", "hamza"];
+// let j = 0;
+
+// do {
+//     console.log(studentsName[j]);
+//     j++;
+// } while (j < studentsName.length)
